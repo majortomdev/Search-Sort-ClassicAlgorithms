e47afec56f54f31cd0616d84af1ccfcbb7003cb0
Joseph Kinahan <kinahanj25@gmail.com>
2024-01-18 19:31:26 +0000

    recursive binsearch version - joys of big O log n

--- src/BinarySearchUsingRecursion/TheSearch.java
+++ src/BinarySearchUsingRecursion/TheSearch.java
@@ -2,9 +2,10 @@ package BinarySearchUsingRecursion;
 
 public class TheSearch {
     public static void main(String[] args) {
-        int[] testBucket = {2,6,16,26,32,33,51,68,80};
+        int[] testBucket = {2,6,16,26,32,33,51,68,80,91,92,95,99,
+                120,125,128,133,135,150};
         System.out.println(binSearch(testBucket, 80));
-
+        System.out.println(recursiveBinSearch(testBucket,0,testBucket.length-1,120));
     }
     //just a plain ol binary search first for old times sake
     public static int binSearch(int[] carton, int token){
@@ -21,5 +22,21 @@ public class TheSearch {
             }
         }
         return -1;
+    }
+
+    public static int recursiveBinSearch(int[] box, int st, int end, int token){
+        System.out.println("[ "+st+"..."+(st + end) / 2+"..."+end+" ]");//
+        if(st>end){
+            return -1;
+        }else {
+            int chop = (st + end) / 2;
+            if (box[chop] == token) {
+                return chop;
+            } else if (box[chop] > token) {
+                   return recursiveBinSearch(box, st, chop - 1, token);
+            } else {
+                   return recursiveBinSearch(box, chop + 1, end, token);
+            }
+        }
     }
 }
