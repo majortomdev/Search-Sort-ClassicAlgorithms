5174c46a7dbfb4058fb515260caa25a8936e71aa
Joseph Kinahan <kinahanj25@gmail.com>
2024-01-25 16:08:45 +0000

    get min and get max values from tree..

--- src/binarySearchTree/App.java
+++ src/binarySearchTree/App.java
@@ -5,9 +5,15 @@ public class App {
     public static void main(String[] args) {
         BST tree = new BST();
         tree.insert(10, "Ten");
+        tree.insert(23, "Twentythree");
         tree.insert(20, "Twenty");
         tree.insert(15, "Fifteen");
-
+        tree.insert(5,"Five");
+        tree.insert(89,"Eightynine");
+        System.out.println("The minimum in our tree is: "+tree.findMinimumNode().key+
+                "->"+tree.findMinimumNode().value);
+        System.out.println("The maximum in our tree is: "+tree.findMaximumNode().key+
+                "->"+tree.findMaximumNode().value);
 
     }
 
--- src/binarySearchTree/BST.java
+++ src/binarySearchTree/BST.java
@@ -13,7 +13,8 @@ public class BST {
             Node current = root;//these must exist before the meaty while loop
             Node parent;
 
-            while (true){
+            while (true){//we keep looping here, checking node values until we get to
+                //empty left or right node as required by key
                 parent = current;
                 if(key< current.key){//we go down left half of big tree
                     current = current.leftChild;//next we check has current a leftnode
@@ -30,8 +31,28 @@ public class BST {
                 }
             }
         }
+    }
 
+    public Node findMinimumNode(){
+        Node current = root;
+        Node last = null;
 
+        while(current != null){
+            last = current;
+            current = current.leftChild;
+        }
+        return last;
+    }
+
+    public Node findMaximumNode(){
+        Node current = root;
+        Node last = null;
+
+        while(current!=null){
+            last = current;
+            current = current.rightChild;
+        }
+        return last;
     }
 
 }
